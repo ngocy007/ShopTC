b44b59031e6956c02f93df1d025a696046b2f726
ngocy007 <dev.yngoc@gmail.com>
2022-06-05 21:03:30 +0700

    Add project files.

---
+++ ShopTC/Controllers/DangNhapController.cs
@@ -0,0 +1,144 @@
+﻿using ShopTC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ShopTC.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        private QLBHTCEntities db = new QLBHTCEntities();
+        public bool CheckUser(string username, string password)
+        {
+            var kq = db.Users.Where(x => x.Email == username && x.MatKhau == password).ToList();
+
+            if (kq.Count() > 0)
+            {
+                Session["HoTen"] = kq.First().TenUser;
+                Session["ID"] = kq.First().MaUser;
+                if (kq.First().admin == true)
+                {
+                    Session["Admin"] = kq.First().admin;
+                }
+                return true;
+            }
+            else
+            {
+                Session["HoTen"] = null;
+                return false;
+            }
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+        public ActionResult DangNhap(string Email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CheckUser(Email, password))
+                {
+                    FormsAuthentication.SetAuthCookie(Email, true);
+                    return RedirectToAction("Index", "ThuCungs");
+                }
+                else
+                    ModelState.AddModelError("", "Tên đăng nhập hoặc tài khoản không đúng.");
+            }
+            var u = new Users();
+            u.MatKhau = password;
+            u.Email = Email;
+            return View(u);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            FormsAuthentication.SignOut();
+            return RedirectToAction("DangNhap", "DangNhap");
+        }
+        // GET: DangNhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DangNhap/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DangNhap/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DangNhap/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DangNhap/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DangNhap/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DangNhap/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DangNhap/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShopTC/Controllers/ThuCungsController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopTC.Models;
+
+namespace ShopTC.Controllers
+{
+    public class ThuCungsController : Controller
+    {
+        private QLBHTCEntities db = new QLBHTCEntities();
+
+        // GET: ThuCungs
+        public ActionResult Index()
+        {
+            if (Session["ID"] == null || Session["ID"].ToString() == "")
+            {
+                return RedirectToAction("Index2", "ThuCungs");
+
+            }
+            else
+            {
+                var thuCungs = db.ThuCung.Include(t => t.LoaiThuCung);
+                ViewBag.MaLoai = new SelectList(db.LoaiThuCung, "MaLoai", "TenLoai");
+                return View(thuCungs.ToList());
+
+            }
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        // GET: ThuCungs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuCung thuCung = db.ThuCung.Find(id);
+            if (thuCung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thuCung);
+        }
+
+        // GET: ThuCungs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLoai = new SelectList(db.LoaiThuCung, "MaLoai", "TenLoai");
+            ViewBag.MaCC = new SelectList(db.NHACC, "MaCC", "TenCC");
+            return View();
+        }
+
+        // POST: ThuCungs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTC,TenTC,GiaTC,SoLuong,GT,MoTa,HinhAnhMH,MaLoai,MaCC,NgaySinh")] ThuCung thuCung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ThuCung.Add(thuCung);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLoai = new SelectList(db.LoaiThuCung, "MaLoai", "TenLoai", thuCung.MaLoai);
+            ViewBag.MaCC = new SelectList(db.NHACC, "MaCC", "TenCC", thuCung.MaCC);
+            return View(thuCung);
+        }
+
+        // GET: ThuCungs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuCung thuCung = db.ThuCung.Find(id);
+            if (thuCung == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLoai = new SelectList(db.LoaiThuCung, "MaLoai", "TenLoai", thuCung.MaLoai);
+            ViewBag.MaCC = new SelectList(db.NHACC, "MaCC", "TenCC", thuCung.MaCC);
+            return View(thuCung);
+        }
+
+        // POST: ThuCungs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTC,TenTC,GiaTC,SoLuong,GT,MoTa,HinhAnhMH,MaLoai,MaCC,NgaySinh")] ThuCung thuCung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thuCung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLoai = new SelectList(db.LoaiThuCung, "MaLoai", "TenLoai", thuCung.MaLoai);
+            ViewBag.MaCC = new SelectList(db.NHACC, "MaCC", "TenCC", thuCung.MaCC);
+            return View(thuCung);
+        }
+
+        // GET: ThuCungs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuCung thuCung = db.ThuCung.Find(id);
+            if (thuCung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thuCung);
+        }
+
+        // POST: ThuCungs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ThuCung thuCung = db.ThuCung.Find(id);
+            db.ThuCung.Remove(thuCung);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
